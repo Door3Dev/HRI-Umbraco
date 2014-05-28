f8d4a1d138502b0b48c6354d03e7f2e8ed9e1d30
Eugene Zharkov <eu.zharkov@gmail.com>
2014-05-29 00:43:34 +0300

    Add Provider and Broker Index pages

---
+++ src/Umbraco.Web.UI/Controllers/ProvidersController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Umbraco.Web.Mvc;
+
+namespace Umbraco.Web.UI.Controllers
+{
+  public class ProvidersController : SurfaceController
+  {
+    public ActionResult Index()
+    {
+      return PartialView("Index");
+    }
+  }
+}
