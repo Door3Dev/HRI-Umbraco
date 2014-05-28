f7e1d60f0a053e8b67fc62e6141e427b6425d471
Eugene Zharkov <eu.zharkov@gmail.com>
2014-05-29 00:17:57 +0300

    Add AboutUs pages

---
+++ src/Umbraco.Web.UI/Controllers/AboutUsController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Umbraco.Web.Mvc;
+
+namespace Umbraco.Web.UI.Controllers
+{
+  public class AboutUsController : SurfaceController
+  {
+    public ActionResult Index()
+    {
+      return PartialView("WeAreDifferent");
+    }
+
+    public ActionResult WeAreDifferent()
+    {
+      return PartialView("WeAreDifferent");
+    }
+
+    public ActionResult OurBoard()
+    {
+      return PartialView("OurBoard");
+    }
+
+    public ActionResult OurStaff()
+    {
+      return PartialView("OurStaff");
+    }
+    
+    public ActionResult Careers()
+    {
+      return PartialView("Careers");
+    }
+  }
+}
