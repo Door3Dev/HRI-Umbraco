b9310406d637a9e3ecc5a45ad74743db66f8033e
Matthew Wood <mattwood2855@gmail.com>
2014-05-29 07:08:21 -0700

    Set up MasterPage template

---
+++ src/Umbraco.Web.UI/Controllers/ValuesController1.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Web.Mvc;
+using Umbraco.Web.Mvc;
+using Umbraco.Web.UI.Models;
+
+namespace Umbraco.Web.UI.Controllers
+{
+    public class ContactFormSurfaceController : SurfaceController
+    {
+        public ActionResult Index()
+        {
+            return PartialView("ContactForm", new ContactFormViewModel());
+        }
+    }
+}
