275e6b3b4c1efc5a06e98d3427c40c959ca45055
Matthew Wood <mattwood2855@gmail.com>
2014-05-30 09:28:06 -0700

    Added packages for ASP Identity and new doc types

---
+++ src/Umbraco.Web.UI/Controllers/IdentitySurfaceController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Web.Mvc;
+using Umbraco.Web.Models;
+using Umbraco.Web.Mvc;
+using Umbraco.Web.UI.Models;
+
+namespace Umbraco.Web.UI.Controllers
+{
+    public class IdentitySurfaceController : SurfaceController
+    {
+        public ActionResult Index(RenderModel model)
+        {
+            return View("Login", new LoginViewModel());
+        }
+
+        public ActionResult Login()
+        {
+            return PartialView("Login", new LoginViewModel());
+        }
+    }
+}
