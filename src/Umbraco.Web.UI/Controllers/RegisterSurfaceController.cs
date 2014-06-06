c5a1bb7e4edc0c8a8b8a0be4b9b1c2f0b123f2fd
Matthew Wood <mattwood2855@gmail.com>
2014-06-06 09:08:31 -0700

    Routing and registration controller

---
+++ src/Umbraco.Web.UI/Controllers/RegisterSurfaceController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Web;
+using System.Web.Mvc;
+using Umbraco.Web.Mvc;
+using Umbraco.Web.UI.Models;
+
+namespace Umbraco.Web.UI.Controllers
+{
+    public class RegisterSurfaceController : SurfaceController
+    {
+        public ActionResult Index()
+        {
+            return RedirectToUmbracoPage(1056);
+        }
+
+        [HttpPost]
+        public ActionResult Register(RegisterViewModel model)
+        {
+            if(ModelState.IsValid)
+            {               
+                return RedirectToUmbracoPage(model.RegistrationSuccessPage);                  
+            }
+            return CurrentUmbracoPage();
+        }
+    }
+}
