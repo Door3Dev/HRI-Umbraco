8357f1c3650f41853e82b86422a0ad381e798788
Matthew Wood <mattwood2855@gmail.com>
2014-06-10 09:13:50 -0700

    About Surface Page

---
+++ src/Umbraco.Web.UI/Controllers/AboutSurfaceController.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Mail;
+using System.Web;
+using System.Web.Mvc;
+using Umbraco.Web.Mvc;
+using Umbraco.Web.UI.Models;
+
+namespace Umbraco.Web.UI.Controllers
+{
+    public class AboutSurfaceController : SurfaceController
+    {
+        [HttpGet]
+        public ActionResult ContactForm()
+        {
+            return View("ContactForm", "MasterPage", new ContactFormViewModel());
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult HandleContactForm(ContactFormViewModel model)
+        {
+            if (!ModelState.IsValid)
+            {
+                return CurrentUmbracoPage();
+            }
+
+            MailMessage email = new MailMessage("matthew.wood@door3.com", "matthew.wood@door3.com");
+            email.Subject = "HRI - Contact Us";
+            email.Body = model.Message + "\n\n" + model.Email;
+
+            try
+            {
+                SmtpClient smtp = new SmtpClient();
+                smtp.Send(email);
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+
+            TempData["IsSuccessful"] = true;
+            return RedirectToCurrentUmbracoPage();
+
+        }
+    }
+}
