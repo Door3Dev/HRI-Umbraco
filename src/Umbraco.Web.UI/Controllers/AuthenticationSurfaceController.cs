cb291a5d8f9bfa88782807c09503c3a56b8ed411
Matthew Wood <mattwood2855@gmail.com>
2014-06-13 09:25:31 -0700

    Began adding authentication

---
+++ src/Umbraco.Web.UI/Controllers/AuthenticationSurfaceController.cs
@@ -0,0 +1,65 @@
+﻿using Microsoft.AspNet.Identity;
+using Microsoft.Owin.Security;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Web;
+using System.Web.Mvc;
+using Umbraco.Web.Mvc;
+using Umbraco.Web.UI.Models;
+
+namespace Umbraco.Web.UI.Controllers
+{
+    public class AuthenticationSurfaceController : SurfaceController
+    {
+        IAuthenticationManager Authentication
+        {
+            get { return HttpContext.GetOwinContext().Authentication; }
+        }
+
+        [HttpGet]
+        public ActionResult Show()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Login(LoginViewModel input)
+        {
+            if (ModelState.IsValid)
+            {
+                if (input.HasValidUsernameAndPassword)
+                {
+                    var identity = new ClaimsIdentity(new[] {
+                            new Claim(ClaimTypes.Name, input.Username),
+                        },
+                        DefaultAuthenticationTypes.ApplicationCookie,
+                        ClaimTypes.Name, ClaimTypes.Role);
+
+                    // if you want roles, just add as many as you want here (for loop maybe?)
+                    identity.AddClaim(new Claim(ClaimTypes.Role, "guest"));
+                    // tell OWIN the identity provider, optional
+                    // identity.AddClaim(new Claim(IdentityProvider, "Simplest Auth"));
+
+                    Authentication.SignIn(new AuthenticationProperties
+                    {
+                        IsPersistent = input.RememberMe
+                    }, identity);
+
+                    return RedirectToAction("index", "home");
+                }
+            }
+
+            return View("show", input);
+        }
+
+        [HttpGet]
+        public ActionResult Logout()
+        {
+            Authentication.SignOut(DefaultAuthenticationTypes.ApplicationCookie);
+            return RedirectToAction("login");
+        }
+    }
+}
