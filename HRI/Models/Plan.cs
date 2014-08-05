d4a5d409c16cac4c3e89461eccaf096d1e038bd6
Eugene Zharkov <eu.zharkov@gmail.com>
2014-08-05 12:31:49 -0700

    Fix referrences

---
+++ HRI/Models/Plan.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace HRI.Models
+{
+    public class Plan
+    {
+        public string HiosId;
+        public string MetalTier;
+        public double RateFactor;
+        public double Price;
+    }
+}
