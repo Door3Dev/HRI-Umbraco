d135c60039679e7e53c5f862926b6498de6e8714
Eugene Shalyuk <eugene.shalyuk@door3.com>
2014-09-15 11:38:51 +0300

    HR-174 - Contact Us - add "Other" as a category

--- HRI/Models/ContactFormViewModel.cs
+++ HRI/Models/ContactFormViewModel.cs
@@ -21,7 +21,8 @@ namespace HRI.Models
                 "Claims",
                 "Cancellation",
                 "Update Information",
-                "Pharmacy"
+                "Pharmacy",
+                "Other"
             };
         }
 
