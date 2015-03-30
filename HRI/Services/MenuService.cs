c8e43b31d7d2cce278cd826e62dd5192d3d2676e
Eugene Zharkov <eu.zharkov@gmail.com>
2015-03-30 15:35:36 +0300

    HR-362 User roles based menu fix

--- HRI/Services/MenuService.cs
+++ HRI/Services/MenuService.cs
@@ -1,7 +1,9 @@
 ﻿using System.Collections.Generic;
 using System.Linq;
-using HRI.Controllers;
+using System.Web.Security;
 using HRI.Models;
+using umbraco.cms.businesslogic.web;
+using Umbraco.Core;
 using Umbraco.Core.Models;
 using Umbraco.Web;
 using Umbraco.Web.Models;
@@ -11,19 +13,20 @@ namespace HRI.Services
     public class MenuService
     {
         /// <summary>
-        /// Umbraco node(page) access radio button list values.
+        /// Umbraco page access radio button list values.
         /// The values are come from the database.
         /// </summary>
         private enum NodeAccess { Public = 9, Members = 10, PublicAndMembers = 11 }
 
         /// <summary>
-        /// Umbraco node(page) visibility radio button list.
+        /// Umbraco page visibility radio button list.
         /// The values are come from the database.
         /// </summary>
         private enum NodeVisibility { Header = 12, Footer = 13,  HeaderAndFooter = 14, NotVisible = 15 }
 
         private readonly UmbracoHelper _helper = new UmbracoHelper(UmbracoContext.Current);
         private MenuType _menuType;
+        private string[] _currentMemberRoles;
 
         /// <summary>
         /// List menu items
@@ -34,6 +37,8 @@ namespace HRI.Services
         {
             // Save the menu type
             _menuType = menuType;
+            // Get current member roles
+            _currentMemberRoles = Roles.GetRolesForUser();
             // Take the home page
             var homePage = ((DynamicPublishedContentList)_helper.ContentAtRoot()).First();
             // Search for the pages that current user has an access
@@ -73,27 +78,33 @@ namespace HRI.Services
         }
 
         /// <summary>
-        /// Checks user rights for the node(page)
+        /// Checks user rights for the page
         /// </summary>
         /// <param name="content">Content item</param>
         /// <returns>Does the user has an access</returns>
         private bool UserHasAccess(IPublishedContent content)
         {
-            // Read the Navigation Visibility node(page) property
+            // Read the Navigation Visibility page property
             var visibility = content.GetPropertyValue<int>("navigationVisibility");
-            // Read the Access Availability node(page) property
+            // Read the Access Availability page property
             var access = content.GetPropertyValue<int>("accessAvailability");
             // Choose what is restricted for the current user (member or anonymous)
             // Nodes(pages) that are accessible for all will be always included
             var resctrictedAccess = _helper.MemberIsLoggedOn() ? (int)NodeAccess.Public : (int)NodeAccess.Members;
-            // Choose allowed node(page) visibility requered during listing
+            // Choose allowed page visibility requered during listing
             var allowedVisibility = _menuType == MenuType.Header ? NodeVisibility.Header : NodeVisibility.Footer;
+            // Check is page protected
+            var isProtected = Access.IsProtected(content.Id, content.Path); 
+            // Get page access roles
+            var roles = isProtected ? Access.GetAccessingMembershipRoles(content.Id, content.Path) : null;
+            // Detect if the user has an access to the page
+            var hasAccess = !isProtected || roles.ContainsAny(_currentMemberRoles);
 
             // Page should have a proper user access (public, members only, etc.)
             // Page should have all Umbraco's rights (role based access)
             // Page should have a proper visibility (header or footer menu)
             return access != resctrictedAccess
-                    && content.HasAccess()
+                    && hasAccess
                     && (visibility == (int) allowedVisibility || visibility == (int)NodeVisibility.HeaderAndFooter);
         }
     }
