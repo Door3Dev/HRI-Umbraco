8e53e2633b86d795f930cba6746bcce4dc13b1cd
Eugene Zharkov <eu.zharkov@gmail.com>
2015-04-07 10:07:34 -0700

    HR-326 Create a notifications area: Important Messages and Announcements

---
+++ HRI/Models/MemberAnnouncement.cs
@@ -0,0 +1,9 @@
+namespace HRI.Models
+{
+    public class MemberAnnouncement
+    {
+        public string Title { get; set; }
+        public string ShortMessage { get; set; }
+        public string FullMessage { get; set; }
+    }
+}
---
+++ HRI/Services/MemberAnnouncementsService.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using HRI.Models;
+using Umbraco.Web;
+using Umbraco.Web.Models;
+
+namespace HRI.Services
+{
+    public class MemberAnnouncementsService
+    {
+        private readonly UmbracoHelper _helper = new UmbracoHelper(UmbracoContext.Current);
+
+        public IEnumerable<MemberAnnouncement> GetList()
+        {
+            // Get the list of announcements that are visible
+            var result =
+                ((DynamicPublishedContent) _helper.ContentAtRoot().First())
+                    .DescendantsOrSelf("MemberCenterAnnouncementItem")
+                    .Where(_ => _.GetPropertyValue<bool>("visible"))
+                    .Select(_ => new MemberAnnouncement()
+                    {
+                        Title = _.GetPropertyValue<string>("title"),
+                        ShortMessage = _.GetPropertyValue<string>("shortMessage"),
+                        FullMessage = _.GetPropertyValue<string>("fullMessage"),
+                    });
+
+            return result;
+        }
+    }
+}
