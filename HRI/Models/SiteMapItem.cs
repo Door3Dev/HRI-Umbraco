cffe08f3928ef7df357394cb82194d0b6f690b64
Eugene Zharkov <eu.zharkov@gmail.com>
2015-08-21 18:24:07 +0300

    HR-239 ability to suppress some pages from the sitemap

---
+++ HRI/Models/SiteMapItem.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+
+namespace HRI.Models
+{
+    public class SiteMapItem
+    {
+        public int Level { get; set; }
+        public string Name { get; set; }
+        public string Url { get; set; }
+        public int Id { get; set; }
+        public int ParentId { get; set; }
+
+        public IList<SiteMapItem> Children;
+    }
+}
---
+++ HRI/Services/SiteMapService.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using HRI.Models;
+using Umbraco.Web;
+using Umbraco.Web.Models;
+using EnumerableExtensions = Umbraco.Core.EnumerableExtensions;
+
+namespace HRI.Services
+{
+    public class SiteMapService
+    {
+        private readonly UmbracoHelper _helper = new UmbracoHelper(UmbracoContext.Current);
+
+        public IList<SiteMapItem> List(int level)
+        {
+            var home = ((DynamicPublishedContent) _helper.ContentAtRoot().First());
+            var flatList = home.Descendants()
+                .Where(_ => _.Visible && _.Level <= level && !_.GetPropertyValue<bool>("hideInSitemap"))
+                .Select(x => new SiteMapItem()
+                {
+                    Id = x.Id,
+                    ParentId = x.Parent.Id,
+                    Level = x.Level,
+                    Name = x.Name,
+                    Url = x.Url
+
+                })
+                .ToList();
+            var result = BuildTree(flatList, home.Id);
+
+            return result;
+        }
+
+        /// <summary>
+        /// Recursive menu builder
+        /// </summary>
+        /// <param name="flatList">Original flat menu items list</param>
+        /// <param name="parentId">Parent node ID</param>
+        /// <returns>Hierarchical list</returns>
+        private IList<SiteMapItem> BuildTree(IList<SiteMapItem> flatList, int parentId)
+        {
+            var list = new List<SiteMapItem>();
+            EnumerableExtensions.ForEach(flatList.Where(x => x.ParentId == parentId), list.Add);
+            list.ForEach(x => x.Children = BuildTree(flatList, x.Id));
+            return list;
+        }
+    }
+}
