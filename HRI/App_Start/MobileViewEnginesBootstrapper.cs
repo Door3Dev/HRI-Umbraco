4d994000cf152f818ef3da481ffcf15113894b96
Eugene Zharkov <eu.zharkov@gmail.com>
2015-08-28 18:14:49 +0300

    HR-428 Merge Umbraco v7.2.8

---
+++ HRI/App_Start/MobileViewEnginesBootstrapper.cs
@@ -0,0 +1,12 @@
+using System.Web.Mvc;
+using MobileViewEngines.MVC3;
+ 
+[assembly: WebActivator.PreApplicationStartMethod(typeof(HRI.MobileViewEngines), "Start")]
+namespace HRI {
+    public static class MobileViewEngines{
+        public static void Start() 
+        {
+            ViewEngines.Engines.Insert(0, new MobileCapableRazorViewEngine());
+        }
+    }
+}
